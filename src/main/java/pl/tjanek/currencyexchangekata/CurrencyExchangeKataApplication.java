5f17738783e805d5692bbaf75921456ecffe7e5a
Tomasz Janek <janek.tomasz@gmail.com>
2023-07-31 17:22:48 +0200

    initial commit

---
+++ src/main/java/pl/tjanek/currencyexchangekata/CurrencyExchangeKataApplication.java
@@ -0,0 +1,13 @@
+package pl.tjanek.currencyexchangekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tjanek/currencyexchangekata/CurrencyExchangeKataApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tjanek.currencyexchangekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
